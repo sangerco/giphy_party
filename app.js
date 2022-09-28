2880b8828ca3599dd977a6cb0fb3c559c6d04381
sangerco <112724308+sangerco@users.noreply.github.com>
2022-09-28 10:43:43 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function addGif(res) {
   if (num) {
     let idx = Math.floor(Math.random() * num);
     let newDiv = document.createElement('div');
-    newDiv.classList.add('deleteImg')
+    newDiv.classList.add('gifContainer')
     let imgDiv = document.createElement('div');
     imgDiv.classList.add('imgDiv')
     imgDiv.innerHTML += `<img src=${res.data[idx].images.original.url}>`;
@@ -37,6 +37,6 @@ $("form").on("submit", async function(evt) {
 // const imgDiv = document.querySelector(".imgDiv")
 
 $('#remove').on("click", function() {
-    $('.deleteImg').remove();
+    $('.gifContainer').remove();
 });
 
